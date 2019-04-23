5d2de3c75e67e03e7983c7da84a1fefd9fd42182
ColineFardel <coline.fardel@gmail.com>
2019-04-23 16:10:46 +0200

    Journée 1 travail de 14h a 16h

--- HomeFrame.java
+++ HomeFrame.java
@@ -5,11 +5,42 @@ public class HomeFrame extends JFrame{
 	private JPanel trucEnHaut = new JPanel();
 	private JPanel trucAuMilieu = new JPanel();
 	private JPanel trucEnBas = new JPanel();
+	private JLabel test1 = new JLabel("test1");
+	private JLabel test2 = new JLabel("test2");
+	private JLabel test3 = new JLabel("test3");
+	private final int LARGEUR = 450;
+	
+	private GridBagConstraints prout = new GridBagConstraints();
+	
 	public HomeFrame() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new GridBagLayout());
-		add(trucEnHaut);
-		add(trucAuMilieu);
-		add(trucEnBas);
+		
+		trucEnHaut.add(test1);
+		trucAuMilieu.add(test2);
+		trucEnBas.add(test3);
+		
+		prout.gridx = 0;
+		prout.gridy = 0;
+		trucEnHaut.setPreferredSize(new Dimension(LARGEUR,50));
+		trucEnHaut.setOpaque(true);
+		trucEnHaut.setBackground(Color.BLUE);
+		add(trucEnHaut,prout);
+		
+		prout.gridx = 0;
+		prout.gridy = 2;
+		trucAuMilieu.setPreferredSize(new Dimension(LARGEUR,600));
+		trucAuMilieu.setOpaque(true);
+		trucAuMilieu.setBackground(Color.ORANGE);
+		add(trucAuMilieu,prout);
+		
+		prout.gridx = 0;
+		prout.gridy = 3;
+		trucEnBas.setPreferredSize(new Dimension(LARGEUR,50));
+		trucEnBas.setOpaque(true);
+		trucEnBas.setBackground(Color.BLACK);
+		add(trucEnBas,prout);
+		
+		pack();
 	}
 }
---
+++ TestHome.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class TestHome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new HomeFrame();
+		frame.setVisible(true);
+
+	}
+
+}
