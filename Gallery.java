3167ecf143630a2eeda1fee6bf2021306ab76f22
Izmoreno <ismael.moreno@students.hevs.ch>
2019-04-29 11:54:54 +0200

    Réparation du document

--- Gallery.java
+++ Gallery.java
@@ -1,4 +1,17 @@
 
-public class Gallery {
+public class Gallery extends JFrame{
+private JLabel labelTitre = new JLabel("Mes photos");
+private JLabel labelPhoto = new JLabel();
+
+public Gallery() {
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	add(labelTitre, GridLayout.NORTH);
+	add(labelPhoto, GridLayout.CENTER);
+	
+	pack();
+	
+}
+	
 
 }
