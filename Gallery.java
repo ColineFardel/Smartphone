74d157314e4ba9f1ea82bdb1f09bcae970cf6435
Izmoreno <ismael.moreno@students.hevs.ch>
2019-05-13 11:33:41 +0200

    test

--- Gallery.java
+++ Gallery.java
@@ -1,22 +1,48 @@
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
-import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
-public class Gallery extends JFrame{
-private JLabel labelTitre = new JLabel("Mes photos");
-private JLabel labelPhoto = new JLabel();
 
-public Gallery() {
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
-	add(labelTitre, BorderLayout.NORTH);
-	add(labelPhoto, BorderLayout.CENTER);
-	
-	pack();
-	
-}
-	
+public class Gallery extends BaseFrame{
 
+
+protected JLabel galleryLabel = new JLabel("Mes photos");
+
+protected JButton addPictureButton = new JButton(new ImageIcon("C:\\Users\\ismor\\workspace\\Smartphone\\src\\images\\icons"));
+
+protected JScrollPane scrollBar;
+
+protected JPanel screen = new JPanel();
+
+protected JPanel galleryPanel = new JPanel();
+
+/**
+ * Construction de la galerie
+ */
+
+public Gallery(){
+	
+	par.gridx = 0;
+    par.gridy = 1;
+
+    screen.setPreferredSize(new Dimension(LARGEUR,700));
+    screen.setBackground(Color.RED);
+    
+ 
+ 
+    
+
+    screen.add(addPictureButton,BorderLayout.NORTH);
+ 
+
+    add(screen,par);
+      
+
+	}
 }
